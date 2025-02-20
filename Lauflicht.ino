efe556bf5c26e6607a6576c87aaa6c046928b28a
OfficerCone <finn.mysegades@gmail.com>
2025-02-20 14:19:46 +0100

    unfertig Lauflicht.ino

--- Lauflicht.ino
+++ Lauflicht.ino
@@ -1,51 +1,56 @@
+
 #include<array>
 
-const int SER = 0;
-const int RCLK = 1;
-const int SRCLK = 2;
-const int CLEAR = 3;
-const int TEST = 7;
+#define SER 0
+#define RCLK 1
+#define SRCLK 2
+#define CLEAR 3
+#define TEST 7
 
-std::array<int,16> start = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}; // LED D6
-std::array<int,16> stop = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // keine LED
+int direction = 0;
+
+
+std::array<int,8> state_1 = {1, 0, 0, 0, 0, 0, 0, 0}; // LED D2
+std::array<int,8> state_2 = {0, 1, 0, 0, 0, 0, 0, 0}; // LED D3
+std::array<int,8> state_3 = {0, 0, 1, 0, 0, 0, 0, 0}; // LED D4
+std::array<int,8> state_4 = {0, 0, 0, 1, 0, 0, 0, 0}; // LED D5
+std::array<int,8> state_5 = {0, 0, 0, 0, 1, 0, 0, 0}; // LED D6
 
 void setup() {
- pinMode(0, OUTPUT); 
- pinMode(1, OUTPUT); 
- pinMode(2, OUTPUT); 
- pinMode(3, OUTPUT); 
- pinMode(7, OUTPUT);
+ pinMode(SER, OUTPUT); 
+ pinMode(RCLK, OUTPUT); 
+ pinMode(SRCLK, OUTPUT); 
+ pinMode(CLEAR, OUTPUT); 
+ pinMode(TEST, OUTPUT);
+ updateLEDs();
+
 }
-void run() {
+
+void updateLEDs() {
   digitalWrite(CLEAR, LOW);
   digitalWrite(CLEAR, HIGH);
   digitalWrite(RCLK, LOW);
 
-  for (int i= 0; i < 16; i++) {
+  for (int i= 0; i < 8; i++) {
    digitalWrite(SRCLK, LOW);
-   digitalWrite(SER, start[i]);
+   digitalWrite(SER, state_1[i]);
    digitalWrite(SRCLK, HIGH);
    }
   digitalWrite(RCLK, HIGH);
-  delay(1000);
   
-  digitalWrite(CLEAR, LOW);
-  digitalWrite(CLEAR, HIGH);
-  digitalWrite(RCLK, LOW);
-  
-  for (int i = 0; i < 16; i++) {
-   digitalWrite(SRCLK, LOW);
-   digitalWrite(SER, stop[i]);
-   digitalWrite(SRCLK, HIGH);
-  }
- 
- 
-digitalWrite(RCLK, HIGH);
-delay(1000);
 
 }
 
 void loop() {
 
-run();
-}
+  for (int i= 0; i < 8; i++) {
+   digitalWrite(SRCLK, LOW);
+   digitalWrite(SER, state_1[i]);
+   digitalWrite(SRCLK, HIGH);
+   }
+  digitalWrite(RCLK, HIGH);
+  delay(200);
+  updateLEDs();
+
+
+} 
